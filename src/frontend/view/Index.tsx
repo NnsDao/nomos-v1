42c42154d610bcc09c5d71d6594ff0607a5b8814
suno <42191310+DevSuno@users.noreply.github.com>
2021-12-13 14:18:17 +0800

    style: home Index
    style: home Index

--- src/frontend/view/Index.tsx
+++ src/frontend/view/Index.tsx
@@ -18,8 +18,8 @@ import Statistic from '../assets/home/Statistic.png';
 import window from '../assets/home/window.png';
 import './index.css';
 export default function index() {
-  const activeClass = 'text-white transition delay-150 duration-500';
-  const inactiveClass = 'text-white opacity-50 transform hover:scale-90';
+  const activeClass = 'text-white transition delay-150 duration-500 cursor-pointer';
+  const inactiveClass = 'text-white opacity-50 transform hover:scale-90 cursor-pointer';
   const [link, setLink] = useState('Story');
   const linkList = ['Story', 'WorkFlow', 'FAQs', 'NnsDAO'];
   const daoList = [
@@ -110,7 +110,7 @@ export default function index() {
     <>
       <div className="w-full m-auto bg-primary pb-32">
         <div className="p-6 sticky top-0 bg-primary z-10">
-          <div className="flex justify-between items-center max-w-1200px m-auto">
+          <div className="flex justify-between items-center max-w-1400px m-auto">
             <div className={'flex jsutify-between items-center'}>
               <div className=" flex  space-x-10">
                 {linkList.map((item, index) => (
@@ -131,7 +131,7 @@ export default function index() {
             <div className={'w-32 h-12 rounded-3xl bg-sign text-white flex justify-center items-center'}>{'Sign up'}</div>
           </div>
         </div>
-        <div className="w-screen h-screen home-bg">
+        <div className="w-screen h-screen home-bg ">
           <div className="max-w-1200px flex flex-wrap  m-auto items-center pt-240px">
             <div className={'flex min-w-1200px flex-col justify-content items-start text-white '}>
               <span className={'text-4xl font-mono mb-4'}> Find Your </span>
@@ -155,33 +155,33 @@ export default function index() {
             </div>
           </div>
         </div>
-        <div className={'min-w-1200px flex flex-col justify-content items-start  text-white '}>
+        <div className={'max-w-1200px mx-auto mt-200px flex flex-col justify-content items-start  text-white '}>
           <span className={'text-4xl font-mono mb-4'}>Every DAOs is unique</span>
           <span className={'text-base '}>Join different DAOn's to brainstorm and collide with your own DAOs</span>
-          <div className={'flex min-w-1200px flex justify-between my-24'}>
+          <div className={'flex min-w-1200px flex justify-between mt-24'}>
             {daoList.map(item => (
               <div key={item.url} className={'w-1/5 flex flex-col justify-content items-center transform hover:text-pink-400 hover:scale-125 transition duration-500  '}>
                 <div>
-                  <img src={item.url} alt="" />
+                  <img src={item.url} alt="" width={'224px'} height={'224px'} />
                 </div>
                 <div className={'mt-12'}>{item.text}</div>
               </div>
             ))}
           </div>
         </div>
-        <div className={'min-w-1200px flex flex-col justify-content items-start  text-white '}>
+        <div className={'max-w-1200px m-auto mt-200px flex flex-col justify-content items-start  text-white '}>
           <span className={'text-4xl font-mono mb-4'}>Features</span>
           <span className={'text-base '}>DAOs To Earn</span>
-          <div className={' w-1200px flex flex-wrap justify-between my-24'}>
+          <div className={' w-full flex flex-wrap justify-between mt-24'}>
             {featuresList.map((item, index) => (
               <div
                 key={index}
                 style={{
                   background: ' linear-gradient(180deg, #3A4FE7 0%, #C931B5 100%)',
                 }}
-                className={'w-30 flex flex-wrap flex-col justify-content items-start pl-14 pr-24 pt-16 pb-32 mx-2 my-6'}>
-                <div className={'flex justify-between items-center '}>
-                  <img src={item.url} alt="" />
+                className={'  featuresList-item'}>
+                <div className={'flex items-center '}>
+                  <img src={item.url} alt="" width={'29px'} height={'29px'} />
                   <span className={'text-xl ml-2'}>{item.title}</span>
                 </div>
                 <div className={'mt-16 text-left'}>{item.text}</div>
@@ -189,7 +189,7 @@ export default function index() {
             ))}
           </div>
         </div>
-        <div id="WorkFlow" className={'min-w-1200px flex flex-col justify-center items-start  text-white '}>
+        <div id="WorkFlow" className={'max-w-1200px mx-auto mt-200px flex flex-col justify-center items-start  text-white '}>
           <span className={'text-4xl font-mono mb-4'}>Work Flow</span>
           <span className={'text-base '}>You can be a boss, you just work for yourself. </span>
           <div className={' w-1200px h-500px flex flex-row justify-center items-center  my-24 '}>
@@ -204,13 +204,13 @@ export default function index() {
             <div className={'flex-grow h-500px -ml-4 ' + `${work === 'Architecture' ? 'architecture' : work === 'DAOn' ? 'DAOn' : 'work'}`}></div>
           </div>
         </div>
-        <div className={'min-w-1200px flex flex-col justify-content items-start  text-white '}>
+        <div className={'max-w-1200px m-auto mt-140px flex flex-col justify-content items-start  text-white '}>
           <span className={'text-4xl font-mono mb-4'}>Virtual Reputation Governance</span>
           <div className={' w-1200px flex flex-wrap justify-between my-24'}>
             <div className={'flex-grow h-500px -ml-4 ' + `${work === 'Architecture' ? 'architecture' : work === 'DAOn' ? 'DAOn' : 'work'}`}></div>
           </div>
         </div>
-        <div id="FAQs" className={'min-w-1200px flex flex-col justify-content items-start  text-white '}>
+        <div id="FAQs" className={'max-w-1200px m-auto mt-200px  flex flex-col justify-content items-start  text-white '}>
           <span className={'text-4xl font-mono mb-4'}>FAQs</span>
           <div className={' w-1200px flex flex-wrap justify-between my-24 text-white '}>
             <Collapse defaultActiveKey={['1']} ghost accordion expandIconPosition={'right'}>
@@ -223,10 +223,10 @@ export default function index() {
           </div>
         </div>
 
-        <div className="introduction-wrapper">
-          <div className="flex flex-col justify-center items-center">
-            <div className="text-7xl my-32 w-9/12 ">Introduction à la notion de grille</div>
-            <div className="w-9/12 text-left mb-40">
+        <div className="introduction-wrapper ">
+          <div className="max-w-1200px m-auto flex flex-col justify-center items-center">
+            <div className="text-7xl mt-60 mb-100px max-w-900px">Introduction à la notion de grille</div>
+            <div className="min-w-1000px text-left mb-64 ">
               <p className="mb-6">
                 Bien que les grilles proviennent des imprimés, elles trouvent leur application partout autour de nous, de l'architecture à l'ingénierie. Une grille bien conçue peut fournir une
                 structure et une cohérence, aider à mettre de l'ordre dans le chaos et créer une disposition plus harmonieuse. Les interfaces n'étant plus rigides, nos systèmes de grille ne le sont
@@ -243,13 +243,24 @@ export default function index() {
             </div>
           </div>
 
-          <div className={'w-1/2 flex justify-between items-start  text-white '}>
-            <img src={marketplace} width={'20%'} alt="" />
-            <img src={window} width={'20%'} alt="" />
-            <img src={google} width={'20%'} alt="" />
-            <img src={app} width={'20%'} alt="" />
+          <div className={'w-840px mx-auto flex justify-between items-start  text-white '}>
+            <img src={marketplace} width={'135px'} height={'45px'} alt="" />
+            <img src={window} width={'135px'} height={'45px'} alt="" />
+            <img src={google} width={'135px'} height={'45px'} alt="" />
+            <img src={app} width={'135px'} height={'45px'} alt="" />
           </div>
+          <div className={'w-840px mx-auto mt-200px mb-4 flex justify-between items-start  text-white '}>
+
+            <span className={inactiveClass}>Nomos</span>
+            <span className={inactiveClass}>Story</span>
+            <span className={inactiveClass}>VRG</span>
+            <span className={inactiveClass}>Partners</span>
+          </div>
+
         </div>
+        <div className='w-full h-px  bg-opacity-10 bg-white'></div>
+        <div className="w-1200px mx-auto my-7 text-left text-white pb-5 text-sl opacity-50">@ 2021, NnsDAO Labs</div>
+
       </div>
     </>
   );
