4d35619c51371d980e795efdaf039e63d5608175
rosendo <42633099+rosendolu@users.noreply.github.com>
2021-12-17 09:55:12 +0800

    style:Format

--- src/declarations/ndp/index.js
+++ src/declarations/ndp/index.js
@@ -1,24 +1,24 @@
-import { Actor, HttpAgent } from "@dfinity/agent";
-
+import { Actor, HttpAgent } from '@dfinity/agent';
 // Imports and re-exports candid interface
 import { idlFactory } from './ndp.did.js';
+
 export { idlFactory } from './ndp.did.js';
 // CANISTER_ID is replaced by webpack based on node environment
 export const canisterId = process.env.NDP_CANISTER_ID;
 
 /**
- * 
+ *
  * @param {string | import("@dfinity/principal").Principal} canisterId Canister ID of Agent
  * @param {{agentOptions?: import("@dfinity/agent").HttpAgentOptions; actorOptions?: import("@dfinity/agent").ActorConfig}} [options]
  * @return {import("@dfinity/agent").ActorSubclass<import("./ndp.did.js")._SERVICE>}
  */
- export const createActor = (canisterId, options) => {
+export const createActor = (canisterId, options) => {
   const agent = new HttpAgent({ ...options?.agentOptions });
-  
+
   // Fetch root key for certificate validation during development
-  if(process.env.NODE_ENV !== "production") {
-    agent.fetchRootKey().catch(err=>{
-      console.warn("Unable to fetch root key. Check to ensure that your local replica is running");
+  if (process.env.NODE_ENV !== 'production') {
+    agent.fetchRootKey().catch(err => {
+      console.warn('Unable to fetch root key. Check to ensure that your local replica is running');
       console.error(err);
     });
   }
@@ -30,9 +30,9 @@ export const canisterId = process.env.NDP_CANISTER_ID;
     ...options?.actorOptions,
   });
 };
-  
+
 /**
  * A ready-to-use agent for the ndp canister
  * @type {import("@dfinity/agent").ActorSubclass<import("./ndp.did.js")._SERVICE>}
  */
- export const ndp = createActor(canisterId);
+export const ndp = createActor(canisterId);
