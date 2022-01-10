c6357d306f29aceda030022240318cb73a7a3f79
suno <42191310+DevSuno@users.noreply.github.com>
2022-01-10 10:36:56 +0800

    fix: avatar

--- src/frontend/components/Avatar.tsx
+++ src/frontend/components/Avatar.tsx
@@ -6,6 +6,29 @@ type Prop = {
   width?: string;
 };
 const Index = (prop: Prop) => {
-  return <div className="avatar-wrapper">{prop.url ? <img className="" src={prop.url} width={prop.width} alt="" /> : <img src={nnsAvatar} width="106px" height="106px" alt="" />}</div>;
+  return (
+    <div className="avatar-wrapper">
+      {prop.url ? <img className="" src={prop.url} width={prop.width} alt="" /> : <img src={nnsAvatar} width="106px" height="106px" alt="" />}
+      {/* <div className="avatar-shade"></div>
+      <div className="dialog-wrapper">
+        <div className="dialog-bg">
+          <img className="dialog-close" alt="" />
+          <div className="head"></div>
+          <div className="content">
+            <div className="avator">
+              <img className="v2avatorUrl" src="avatorUrl" alt="" />
+            </div>
+            <div className="input">
+              <input />
+            </div>
+            <div className="footer">
+              <div className="cancel"></div>
+              <div className="ok"></div>
+            </div>
+          </div>
+        </div>
+      </div> */}
+    </div>
+  );
 };
 export default Index;
