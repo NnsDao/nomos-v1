74f1c4cdde079835cc6600d7447881e39e55a147
rosendolu <rosendo2018@163.com>
2022-07-13 09:54:17 +0800

    demo

--- src/frontend/service/index.ts
+++ src/frontend/service/index.ts
@@ -1,8 +1,8 @@
 import nicpActor from '@nnsdao/nnsdao-kit/dip20/types';
 import { _SERVICE as DistributeActor } from '@nnsdao/nnsdao-kit/distribute/types';
 // import { idlFactory as distributeIDL } from '@nnsdao/nnsdao-kit/distribute/index.js';
-import { getActor } from '@nnsdao/nnsdao-kit/helper/agent';
+import { agent, getActor } from '@nnsdao/nnsdao-kit/helper/agent';
 import canister from './config';
-
+agent.replaceIdentity(identity);
 export const getDistributeActor = async (props: any) => getActor({ ...props, ...canister.distribute, needAuth: false }) as unknown as Promise<DistributeActor>;
 export const getNICPActor = async (props: any) => getActor({ ...props, ...canister.nicp, needAuth: false }) as unknown as Promise<nicpActor>;
