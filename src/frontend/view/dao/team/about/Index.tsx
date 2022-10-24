d0cabd905c13714d499f96d7c7bc2376a25ed8f6
neptune <103205025+neptune0xf@users.noreply.github.com>
2022-10-24 22:32:59 +0800

    fix: about page

--- src/frontend/view/dao/team/about/Index.tsx
+++ src/frontend/view/dao/team/about/Index.tsx
@@ -7,7 +7,7 @@ import RichText from '../../../../components/RichText';
 const About = () => {
   const { cid = '' } = useParams();
   const daoInfo = useGetDaoInfo(cid);
-  if (daoInfo.isFetching || daoInfo.data) {
+  if (daoInfo.isFetching || !daoInfo.data) {
     return (
       <Box className="flex justify-center items-center" sx={{ textAlign: 'center' }}>
         <CircularProgress size={24} />
